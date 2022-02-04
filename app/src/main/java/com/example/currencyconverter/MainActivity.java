8f066edb4473db439dbd6c93821272bbf85d9095
Ahmad Fares <ahmaddfaress@Ahmads-MacBook-Air.local>
2022-02-04 17:02:46 +0200

    Added to the bottons OnClick methods and made all the coding required for the process making sure that it recieves and give back decimal number notations, in addition to providing comments for the Toast option of the request only for the programmers reading my code

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 EditText amount;
 TextView P;
+    DecimalFormat df = new DecimalFormat("#.###");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,22 @@ TextView P;
 
         amount =(EditText) findViewById(R.id.amount);
         P=(TextView) findViewById(R.id.textView3);
+    }
+    public void convertToD(View V){
+        double output =Double.parseDouble(amount.getText().toString())/22000;
+
+        P.setText(df.format(output) + "$");
+        // String z= output + "$"
+        // Toast.makeText(getApplicationContext(), output, Toast.LENGTH_SHORT).show();
+
+
+    }
+    public void convertToLL(View V){
+        double output =Double.parseDouble(amount.getText().toString())*22000;
+
+        P.setText(df.format(output) + "LL");
+
+        // String z= output + "LL";
+        //Toast.makeText(getApplicationContext(), z, Toast.LENGTH_SHORT).show();      (In case of toast)
     }
 }
