30c3c1f975fa12d9e4451fc99e45e23f6847f6ad
Ahmad Fares <ahmaddfaress@Ahmads-MacBook-Air.local>
2022-02-04 16:57:51 +0200

    Added a textView to Show the number after the conversion with some coding inicializations

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText amount;
+TextView P;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amount =(EditText) findViewById(R.id.amount);
+        P=(TextView) findViewById(R.id.textView3);
     }
 }
